09af3e2fd0d868d0d6ed1c3efd21fda08da913fc
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-11-04 17:23:37 +0330

    Add shrink functionality

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Task } from '../../Task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -14,9 +16,14 @@ export class AddTaskComponent implements OnInit {
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask: Boolean = false;
+  subscription: Subscription;
 
-
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value) => this.showAddTask = value);
+   }
 
   ngOnInit(): void {
   }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title: string = 'Task tracker';
-  
-  constructor() { }
+  showAddTask: Boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value) => this.showAddTask = value);
+   }
 
   ngOnInit(): void {
     //run code here at component runtime
   }
 
   toggleAddTask() {
-    console.log('Toggle add')
+    this.uiService.toggleAddTask();
   }
 }
