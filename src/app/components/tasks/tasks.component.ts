f2b4e3bcb5cefbc33f0c5dd533e4266faf6162b9
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-11-04 13:40:53 +0330

    load simple tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Task} from '../../Task';
+import {TASKS} from '../../mock-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+
+  tasks: Task[] = TASKS;
 
   constructor() { }
 
