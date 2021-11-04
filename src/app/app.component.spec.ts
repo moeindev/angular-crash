9c1f011f89f7bd875d3feade271e596c49facf5f
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-11-04 12:29:38 +0330

    Basic renaming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-crash'`, () => {
+  it(`should have as title 'Task tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-crash');
+    expect(app.title).toEqual('Task tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-crash app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Task tracker app is running!');
   });
 });
