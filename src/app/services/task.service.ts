9e3260b70fda2ea5c4d6fb04f7fb0496ac179e0d
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-11-04 14:12:24 +0330

    create task service and use it

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Task} from '../Task';
+import {TASKS} from '../mock-tasks';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  getTasks(): Task[] {
+    return TASKS;
+  }
+
+  
+}
